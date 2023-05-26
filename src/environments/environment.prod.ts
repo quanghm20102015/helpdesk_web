c982c4a2b37682d4a80c705b6a6c03eb0860c457
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-05-26 09:30:41 +0700

    edit url web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   env: {
     // HOSTING_API: 'https://localhost:7257/api',
-    WEB_ADDRESS: 'http://103.63.109.82:4200',
+    WEB_ADDRESS: 'http://103.63.109.82:4200/#',
     HOSTING_API: 'http://103.63.109.82:5216/api'
   }
 };
