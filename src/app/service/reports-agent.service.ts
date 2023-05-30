69ef08f68960e1a069f2368ff5cb109cb164d235
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-05-30 07:40:53 +0700

    Ghép api báo cáo agent

--- src/app/service/reports-agent.service.ts
+++ src/app/service/reports-agent.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ServiceInvokerService } from "./service-invoker.service";
+import { Observable } from "rxjs";
+import { AppSettings } from "../constants/app-setting";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportsAgentService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private serviceInvoker: ServiceInvokerService) { }
+
+  
+    topConversationAgent(data: any): Observable<any> {
+      return this.http.get(
+        AppSettings.HostingAddress + '/Reports/TopConversationAgent?fromDate=' + data.fromDate + '&toDate=' + data.toDate + '&idCompany=' + data.idCompany
+      );
+    }
+
+    performentMonitorAgentTotal(data: any): Observable<any> {
+      return this.http.get(
+        AppSettings.HostingAddress + '/Reports/PerformentMonitorAgentTotal?fromDate=' + data.fromDate + '&toDate=' + data.toDate + '&idCompany=' + data.idCompany + '&idUser=' + data.idUser
+      );
+    }
+
+    
+    getPerformanceMonitorAgent(data: any): Observable<any> {
+      return this.http.get(
+        AppSettings.HostingAddress + '/Reports/PerformentMonitorAgent?fromDate=' + data.fromDate + '&toDate=' + data.toDate + '&idCompany=' + data.idCompany + '&type=' + data.type + '&idUser=' + data.idUser
+      );
+    }
+
 }
