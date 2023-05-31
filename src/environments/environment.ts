a5a06c00f792c53c1c6097e09bf2dfacad2c646f
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-05-31 08:52:39 +0700

    edit pai

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     // HOSTING_API: 'https://localhost:7257/api',
     // WEB_ADDRESS: 'https://localhost:4200',
     WEB_ADDRESS: 'http://103.63.109.82:4200/#',
-    HOSTING_API: 'http://103.63.109.79:8200/api'
+    HOSTING_API: 'http://103.63.109.82:5216/api'
   }
 };
 
