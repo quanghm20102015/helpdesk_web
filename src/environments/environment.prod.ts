a875af30778851ac49697b8a34abcf78c8074cfb
LAPTOP-5QT8CTV3\quang <quanghm2010@gmail.com>
2023-07-04 23:17:53 +0700

    config api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   env: {
     // HOSTING_API: 'https://localhost:7257/api',
-    WEB_ADDRESS: 'http://45.118.147.93:4200/#',
-    HOSTING_API: 'http://45.118.147.93:5216/api'
+    WEB_ADDRESS: 'http://103.63.109.82:4200/#',
+    HOSTING_API: 'http://103.63.109.82:5216/api'
   }
 };
