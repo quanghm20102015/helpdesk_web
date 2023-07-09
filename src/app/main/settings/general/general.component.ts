d03f9785a330db9c008efb8aafaddea10cdc3350
minhtmu21 <minhtmu@gmail.com>
2023-07-10 00:48:17 +0700

    Fix setting

--- src/app/main/settings/general/general.component.ts
+++ src/app/main/settings/general/general.component.ts
@@ -9,23 +9,51 @@ import { UserInfoStorageService } from 'src/app/service/user-info-storage.servic
   styleUrls: ['./general.component.css']
 })
 export class GeneralComponent implements OnInit {
-  model: any = { username: '',idUser: 0, dayResolve: 0 }
+  model: any = { username: '', idUser: 0, dayResolve: 0, companyName: this.userInfoStorageService.getCompany(), language: 1, email: this.userInfoStorageService.getWorkemail() }
+  modelChangePassword: any = { passworkOld: '', passwordNew: '', passwordNewConfirm: '' }
+  modelTab3: any = { alertEvent: null, alertTone: '', sendAudio: null, emailNotification: null, pushNotification: null}
+  tab: any = 1
+  note: any = ''
   submitted: boolean = false
-  form: FormGroup = this._fb.group({
+  listLanguage: any = [
+    { id: 1, name: 'English' }
+  ]
+  formTab1: FormGroup = this._fb.group({
     username: [this.model.username, [Validators.required]],
+    companyName: [this.model.companyName],
+    language: [this.model.language],
+    email: [this.model.email],
     dayResolve: [this.model.dayResolve],
   });
+  formProfile: FormGroup = this._fb.group({
+    fullName: [this.model.fullName, [Validators.required]],
+    displayName: [this.model.displayName, [Validators.required]],
+  });
+
+  formChangePassword: FormGroup = this._fb.group({
+    passworkOld: [this.model.passworkOld, [Validators.required]],
+    passwordNew: [this.model.passwordNew, [Validators.required]],
+    passwordNewConfirm: [this.model.passwordNewConfirm, [Validators.required]],
+  })
   constructor(
     private _fb: FormBuilder,
     private router: Router,
     private userInfoStorageService: UserInfoStorageService,
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.model.idUser = +(this.userInfoStorageService.getIdUser() ? this.userInfoStorageService.getIdUser() : 0)
+    this.formTab1.get('email')?.disable();
   }
 
-  get f() {
-    return this.form.controls
+  get f1() {
+    return this.formTab1.controls
+  }
+  get fProfile() {
+    return this.formProfile.controls
+  }
+
+  selectTab(tab: number) {
+    this.tab = tab
   }
 }
